3a13a3c96a7d93fa4f674381f1ecd4597a884bf6
elzzup <guild0105@gmail.com>
2015-01-10 18:00:51 +0900

    split modules

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
 import socket
 
-#host = socket.gethostbyname('lightson.dip.jp')
-#host = socket.gethostbyname('localhost')
-#host = '133.14.234.245'
-host = '133.20.178.81'
+# host = socket.gethostbyname('lightson.dip.jp')
+host = socket.gethostbyname('localhost')
+# host = '133.14.234.245'
+# host = '133.20.178.81'
 port = 4000
+print(len(sys.argv))
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
 
 so = socket.socket()
 so.connect((host, port))
-so.send('{"SSIDs":["TDU_MRCL_WLAN_DOT1X","TDU_MRCL_WLAN","eduroam","TDU_MRCL_GUEST","Buffalo-G-3658"], "twitterID":"hoge"}')
-# "\u0000",
+json_data = '{"SSIDs":["TDU_MRCL_WLAN_DOT1X","TDU_MRCL_WLAN","eduroam","TDU_MRCL_GUEST","Buffalo-G-3658"], "twitterID":"ざっぷ"}'
+so.send(json_data.encode('UTF-8'))
 
 data = []
 while True:
@@ -21,4 +26,4 @@ while True:
 so.close()
 
 data = ''.join(data)
-print data
+print(data)
--- server.py
+++ server.py
@@ -1,26 +1,44 @@
 # -*- coding: utf-8 -*-
-import SocketServer
-import socket
-import json
 
-HOST = socket.gethostbyname('')
-PORT = 4000
+if __name__ == '__main__':
+    import socketserver
+    import socket
+    import json
 
-class Handler(SocketServer.StreamRequestHandler):
-    def handle(self):
+    from handles import handler
+    from models import log_model
+
+    HOST = socket.gethostbyname('')
+    PORT = 4000
+
+    def work(self):
         while True:
-            json_data = self.request.recv(1024)
+            json_data = self.request.recv(1024).strip()
             if len(json_data) == 0:
                 print("emp")
                 break
+            if json_data.__class__.__name__ == "bytes":
+                json_data = json_data.decode('UTF-8')
+            print(json_data.__class__)
             print(json_data)
             data = json.loads(json_data)
+            print(data)
             res = ""
             res += "sn: " + data['twitterID']
             print(res)
-            self.request.send(res)
+            self.request.send(res.encode('UTF-8'))
         self.request.close()
 
-server = SocketServer.TCPServer((HOST, PORT), Handler)
-print 'listening', server.socket.getsockname()
-server.serve_forever()
+    handler.Handler.set_work(work)
+    limit = 1
+    while (limit < 10):
+        limit += 1
+        try:
+            server = socketserver.TCPServer((HOST, PORT), handler.Handler)
+            break
+        except OSError:
+            print("port: " + str(PORT) + " is userd")
+            PORT += 1
+
+    print('listening', server.socket.getsockname())
+    server.serve_forever()
